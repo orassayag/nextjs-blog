86dcab95660514bd48c13e561b05f4ae3b56b64b
Or Assayag <orassayag@gmail.com>
2020-11-27 16:05:02 +0200

    Modify blog project

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+import Head from 'next/head';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
+
+export const getStaticPaths = () => {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false
+    };
+};
+
+export const getStaticProps = async ({ params }) => {
+    const postData = await getPostData(params.id);
+    return {
+        props: {
+            postData
+        }
+    };
+};
+
+const Post = ({ postData }) => {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    );
+};
+
+export default Post;
